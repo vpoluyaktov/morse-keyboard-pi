79d5405a349f665d39a4a475b5d53f6ce71ec239
vpoluyaktov <vpoluyaktov@gmail.com>
2020-02-21 19:21:24 -0800

    Main Form created

--- MainScreen.py
+++ MainScreen.py
@@ -3,30 +3,19 @@
 
 import npyscreen
 from themes.BlueTheme import BlueTheme
-from themes.WhiteTheme import WhiteTheme
+from MainForm import MainForm
 
-class TestApp(npyscreen.NPSApp):
+class App(npyscreen.NPSAppManaged):
+
+    #def onStart(self):
+    #    self.add(npyscreen.TitleText, name = "Text:", value= "Hellow World!" ) 
+
     def main(self):
         npyscreen.setTheme(BlueTheme)
-        # These lines create the form and populate it with widgets.
-        # A fairly complex screen in only 8 or so lines of code - a line for each control.
-        F  = npyscreen.Form(name = "Welcome to Npyscreen")
-        t  = F.add(npyscreen.TitleText, name = "Text:", )
-        fn = F.add(npyscreen.TitleFilename, name = "Filename:",)
-        fn2 = F.add(npyscreen.TitleFilenameCombo, name="Filename2:")
-        dt = F.add(npyscreen.TitleDateCombo, name = "Date:")
-        s  = F.add(npyscreen.TitleSliderPercent, accuracy=0, out_of=12, name = "Slider")
-        ml = F.add(npyscreen.MultiLineEdit, 
-               value = """try typing here!\nMutiline text, press ^R to reformat.\n""", 
-               max_height=5, rely=9)
-        ms = F.add(npyscreen.TitleSelectOne, max_height=4, value = [1,], name="Pick One", 
-                values = ["Option1","Option2","Option3"], scroll_exit=True)
-        ms2= F.add(npyscreen.TitleMultiSelect, max_height =-2, value = [1,], name="Pick Several", 
-                values = ["Option1","Option2","Option3"], scroll_exit=True)
-        
-        # This lets the user play with the Form.
-        F.edit()
+        mainForm = self.addForm("MAIN", MainForm)
+        mainForm.edit()
+
         
 if __name__ == "__main__":
-    App = TestApp()
-    App.run()   
+    app = App()
+    app.run()   
